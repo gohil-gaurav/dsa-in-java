5132cac9ec084b7dad2c008bcb2a1614a4d27b3b
gohil-gaurav <gohilgaurav89@gmail.com>
2026-05-26 11:16:58 +0530

    Add package declaration to DecimalToBinary class and remove from combination class

--- 03-Functions/DecimalToBinary.java
+++ 03-Functions/DecimalToBinary.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class DecimalToBinary {
+    public static int decimalToBinary(int n){
+        int binary = 0;
+        int power = 1;
+        while (n>0) {
+            int lastDigit = n%2;
+            binary += lastDigit*power;
+            power*=10;
+            n/=2;
+        }
+        return binary;
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(decimalToBinary(n));
+    }
 }
