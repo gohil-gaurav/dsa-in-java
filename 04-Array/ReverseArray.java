0268db4cf1fcf2aa26115e30e675db8bd28bcf41
gohil-gaurav <gohilgaurav89@gmail.com>
2026-06-03 06:27:48 +0530

    Add initial structure for ReverseArray and RotateArray classes

--- 04-Array/ReverseArray.java
+++ 04-Array/ReverseArray.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class ReverseArray {
+
+    public static void reverseArray(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        for(int a : arr) {
+            System.out.print(a+" ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 3, 2, 6, 5};
+
+        reverseArray(arr);
+
+    }
 }
--- 04-Array/RotateArray.java
+++ 04-Array/RotateArray.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class RotateArray {
+
+    public static void rotateArr(int[] arr, int d){
+
+        int n = arr.length;
+        d = d % n;
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+        for(int a : arr){
+            System.out.print(a+" ");
+        }
+    }
+
+    public static void reverse(int[] arr, int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int d = 2;
+
+        rotateArr(arr, d);
+    }
 }
